d7b84654a3d296909d752d314d79115e9203c91a
[katslo] <katjaslobodjanac@gmail.com>
2025-07-13 20:08:19 -0300

    Reviso detalles

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-let Invitados = [];
+let items = [];
 
 const formulario = document.getElementById("Formulario");
 const input = document.getElementById("input-texto");
-const lista = document.getElementById("Lista de Invitados");
+const lista = document.getElementById("Lista");
 
 const mostrarLista =() => {
-    lista.innerHTML = Invitados.map((item) =>
-<div class="tarjeta"> ${Invitados} </div>).join;
+    lista.innerHTML = items
+    .map((item) =>
+<div class="tarjeta"> ${item} </div>)
+.join ("");
 }
 
 formulario.addEventListener("submit", (e) => { 
 e.preventDefault()
 const texto =input.value.trim();
 if(texto=== "") return;
-Invitados.push(texto) 
+items.push(texto) 
 input.value = "";
 mostrarLista ();
 })
-
